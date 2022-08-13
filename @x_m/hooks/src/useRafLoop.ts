73a63e37094d6d1ee6ecd2613c0299ac89ef2a67
xiaomingTang <1038761793@qq.com>
2022-08-13 19:11:27 +0800

    lint: prettier

--- @x_m/hooks/src/useRafLoop.ts
+++ @x_m/hooks/src/useRafLoop.ts
@@ -2,13 +2,21 @@ import { useEffect, useRef, useState } from 'react'
 import noop from 'lodash/noop'
 
 export function useRafLoop<T>(callback: (timestamp: number) => T): T
-export function useRafLoop<T>(callback: (timestamp: number) => T, options: {
-  enable?: boolean
-}): T | undefined
-export function useRafLoop<T>(callback: (timestamp: number) => T, options?: {
-  enable?: boolean,
-}) {
-  const initialValueRef = useRef(!options ? callback(performance.now()) : undefined)
+export function useRafLoop<T>(
+  callback: (timestamp: number) => T,
+  options: {
+    enable?: boolean
+  }
+): T | undefined
+export function useRafLoop<T>(
+  callback: (timestamp: number) => T,
+  options?: {
+    enable?: boolean
+  }
+) {
+  const initialValueRef = useRef(
+    !options ? callback(performance.now()) : undefined
+  )
   const [result, setResult] = useState<T | undefined>(initialValueRef.current)
   const callbackRef = useRef(callback)
   const enable = options?.enable ?? true
