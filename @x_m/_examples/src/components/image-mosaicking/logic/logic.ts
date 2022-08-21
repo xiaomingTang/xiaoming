d288c5b37639da13babfaae1e82b63a7d237944c
xiaomingTang <1038761793@qq.com>
2022-08-22 00:44:55 +0800

    feat: try to init Point, Line & Shape

---
+++ @x_m/_examples/src/components/image-mosaicking/logic/logic.ts
@@ -0,0 +1,54 @@
+/* eslint-disable max-classes-per-file */
+// import EventEmitter from "@x_m/event-emitter";
+
+import clamp from 'lodash/clamp'
+
+export class Point {
+  moveable = true
+
+  constraint?: Line
+
+  x: number
+
+  y: number
+
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+
+  moveTo(x: number, y: number) {
+    if (!this.moveable) {
+      return this
+    }
+    if (!this.constraint) {
+      this.x = x
+      this.y = y
+      return this
+    }
+    const [{ x: x1, y: y1 }, { x: x2, y: y2 }] = this.constraint.endpoints
+    const dAB = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    // dAN = (vAM * vAB) / dAB
+    const dAN = ((x - x1) * (x2 - x1) + (y - y1) * (y2 - y1)) / dAB
+    const lamda = clamp(dAN / dAB, 0, 1)
+    this.x = x1 + lamda * (x2 - x1)
+    this.y = y1 + lamda * (y2 - y1)
+    return this
+  }
+}
+
+export class Line {
+  endpoints: [Point, Point]
+
+  constructor(a: Point, b: Point) {
+    this.endpoints = [a, b]
+  }
+}
+
+export class Shape {
+  points: Point[] = []
+
+  constructor(points: Point[]) {
+    this.points = points
+  }
+}
