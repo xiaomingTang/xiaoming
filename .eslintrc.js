098c3c9e3f9f1b00c42bc1be11b1a47e7313b39b
xiaomingTang <1038761793@qq.com>
2022-11-22 22:02:47 +0800

    feat: 移出了展示项目 www

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,64 +22,6 @@ module.exports = {
 
     'import/prefer-default-export': 'off',
   },
-  overrides: [
-    {
-      files: ['@zimi/www/**/*'],
-      extends: [
-        'eslint:recommended',
-        'airbnb-base',
-        'plugin:@typescript-eslint/recommended',
-        'airbnb-typescript/base',
-        'next/core-web-vitals',
-        'prettier',
-      ],
-      settings: {
-        next: {
-          rootDir: '@zimi/www'
-        },
-        "import/resolver": {
-          alias: {
-            map: [
-              ['@', '@zimi/www/src'],
-              ['@ROOT', '@zimi/www'],
-            ],
-          },
-        }
-      },
-      rules: {
-        'no-console': 'off',
-        'react-hooks/exhaustive-deps': 'warn',
-    
-        // tsc 会检查, 无需 lint
-        'no-dupe-class-members': 'off',
-    
-        'import/prefer-default-export': 'off',
-        "import/extensions": [
-           "off",
-        ],
-        // md 此处是以 overrides 的 rootDir 为起点的
-        // "@next/next/no-html-link-for-pages": ["error", 'src/pages'],
-        "@typescript-eslint/no-shadow": ["warn", {
-          ignoreTypeValueShadow: true,
-          ignoreFunctionTypeParameterNameValueShadow: true,
-        }],
-        "@typescript-eslint/no-unused-vars": ["warn", {
-          args: "after-used",
-          argsIgnorePattern: "^_",
-          destructuredArrayIgnorePattern: "^_",
-        }],
-      },
-    },
-    {
-      files: ['@zimi/www/pages/**/*.{md,mdx}'],
-      extends: [
-        'plugin:mdx/recommended',
-      ],
-      settings: {
-        "mdx/code-blocks": true,
-      }
-    },
-  ],
   settings: {
     react: {
       'version': 'detect',
--- @zimi/www/utils/none-image-loader.js
+++
@@ -1,3 +0,0 @@
-export default function NoneImageLoader({ src, width, quality }) {
-  return `${src}?w=${width}&q=${quality || 75}`
-}
