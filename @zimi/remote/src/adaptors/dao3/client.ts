d69964080052ed3903ca7bf396d7aaa86b46f6d8
tangqiulin <tangqiulin@box3.ink>
2025-01-09 11:28:24 +0800

    feat: 新增 dao3 adaptor

---
+++ @zimi/remote/src/adaptors/dao3/client.ts
@@ -0,0 +1,43 @@
+import EventEmitter from 'eventemitter3'
+import { isRemoteAdaptorData, type AdaptorPackageData } from '../../index'
+
+interface RemoteChannel {
+  onClientEvent(fn: (e: unknown) => void): void
+}
+
+/**
+ * 这是 [dao3 游戏](https://dao3.fun/) 代码客户端适配器。
+ * ```ts
+ * const remoteManager = new ClientSideRemoteChannelEventManager(remoteChannel)
+ *
+ * export const remoteAdaptor = {
+ *   every: remoteManager.onEvery.bind(remoteManager),
+ *   off: remoteManager.off.bind(remoteManager),
+ *   on: remoteManager.on.bind(remoteManager),
+ *   once: remoteManager.once.bind(remoteManager),
+ *   emit: (e) => {
+ *     remoteChannel.sendServerEvent(e)
+ *   },
+ * } satisfies Adaptor
+ * ```
+ */
+export class ClientSideRemoteChannelEventManager extends EventEmitter<{
+  [key: string]: [AdaptorPackageData]
+}> {
+  constructor(remoteChannel: RemoteChannel) {
+    super()
+    remoteChannel.onClientEvent((e) => {
+      if (!isRemoteAdaptorData(e)) {
+        return
+      }
+      if (!e.name.startsWith('__REMOTE_VALUE_REQ__')) {
+        this.emit('__remote_every__', e)
+      }
+      this.emit(e.name, e)
+    })
+  }
+
+  onEvery(fn: (args: AdaptorPackageData) => void): void {
+    this.on('__remote_every__', fn)
+  }
+}
