f60d4d8aea8def4bbc77ae907fc337265e4ee39e
tangqiulin <tangqiulin@box3.ink>
2025-01-14 17:56:59 +0800

    feat: 新增 handler 便于在业务中配置自定义 handler

--- @zimi/remote/src/remoteValue/exposeToRemote.ts
+++ @zimi/remote/src/remoteValue/exposeToRemote.ts
@@ -16,11 +16,46 @@ interface ExposeProps {
    * 也可以修改传入的 data
    */
   onRequest?: (e: AdaptorData) => RemoteCallData | Promise<RemoteCallData>
+  /**
+   * 如果你想要自定义处理函数,
+   * 需要谨慎且完善地处理各种 paths, args,
+   * 以避免远程调用时出现未预期的错误。
+   *
+   * ---
+   *
+   * 我的默认处理程序谨供参考:
+   * ``` ts
+   * let target = obj
+   * for (let i = 0; i < data.paths.length; i += 1) {
+   *   target = target[data.paths[i] as keyof typeof target] as typeof target
+   * }
+   * if (target instanceof Function) {
+   *   return target(...data.args)
+   * }
+   * return target
+   * ```
+   *
+   * @returns 返回值将会被发送给远程调用方，可以是立即数，也可以是 Promise（Promise 会 await 后再返回）
+   */
+  handler?: (e: AdaptorData) => unknown
 }
 
 function defaultOnRequest(e: AdaptorData) {
   return e.data
 }
+
+const geneDefaultHandler: <T>(obj: T) => (data: AdaptorData) => unknown =
+  (obj) =>
+  ({ data }) => {
+    let target = obj
+    for (let i = 0; i < data.paths.length; i += 1) {
+      target = target[data.paths[i] as keyof typeof target] as typeof target
+    }
+    if (target instanceof Function) {
+      return target(...data.args)
+    }
+    return target
+  }
 
 export function isRemoteValueEvent(eventName: string) {
   return eventName.startsWith('__REMOTE_VALUE_REQ__')
@@ -31,24 +66,20 @@ export function exposeToRemote<T extends object>(obj: T, options: ExposeProps) {
     globalName,
     adaptor,
     exposeTo,
+    handler: inputHandler,
     onRequest = defaultOnRequest,
   } = options
   const callback = async (e: AdaptorPackageData) => {
     try {
-      const { paths, args } = await onRequest(e as AdaptorData)
+      const data = await onRequest(e as AdaptorData)
       if (!exposeTo.includes(e.deviceId) && !exposeTo.includes('*')) {
         throw new RemoteError('permission denied')
       }
-      let target = obj
-      for (let i = 0; i < paths.length; i += 1) {
-        target = target[paths[i] as keyof typeof target] as typeof target
-      }
-      let res: unknown
-      if (target instanceof Function) {
-        res = await target(...args)
-      } else {
-        res = target
-      }
+      const handler = inputHandler ?? geneDefaultHandler(obj)
+      const res = await handler({
+        ...e,
+        data,
+      })
       adaptor.emit({
         name: e.callbackName ?? 'IMPOSSIBLE_NO_CALLBACK_NAME',
         deviceId: e.targetDeviceId,
