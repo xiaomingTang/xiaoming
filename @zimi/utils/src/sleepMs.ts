e5e66e1f751aa40c9c8507b9a2538ca1dffae2f5
xiaomingTang <1038761793@qq.com>
2025-05-31 04:31:05 +0800

    perf: 优化 2 个函数

--- @zimi/utils/src/sleepMs.ts
+++ @zimi/utils/src/sleepMs.ts
@@ -1,5 +1,12 @@
-export function sleepMs(ms: number) {
-  return new Promise<void>((resolve) => {
-    setTimeout(resolve, ms)
+import { clamp } from 'lodash-es'
+
+const InfiniteTimeout = 2 ** 31 - 1
+
+export async function sleepMs(ms: number): Promise<void> {
+  if (Number.isNaN(ms)) {
+    throw new Error('invalid input: NaN')
+  }
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), clamp(ms, 0, InfiniteTimeout))
   })
 }
