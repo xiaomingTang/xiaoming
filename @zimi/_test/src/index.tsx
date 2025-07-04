0847cdfb0f66b49eada73c9ec89ceb3e77c8bd53
xiaoming <1038761793@qq.com>
2025-07-04 17:08:14 +0800

    fix: runtime error

--- @zimi/_test/src/index.tsx
+++ @zimi/_test/src/index.tsx
@@ -1,5 +1,6 @@
+import './styles/global.css'
+
 import ReactDOM from 'react-dom/client'
-import './styles/global.scss'
 import App from './App'
 
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
